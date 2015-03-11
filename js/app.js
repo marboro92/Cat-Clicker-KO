45b8fc7b3c0db5314ba7bdd4a174e62595b78234
Marianne Borowiak <borowiak.mar@gmail.com>
2015-03-11 11:55:20 -0400

    reorganized to construct multiple cats

--- js/app.js
+++ js/app.js
@@ -1,22 +1,10 @@
-var ViewModel = function() {
-	//keep outer this, less confusinsing, sometimes that is used
-	var self= this;
-	this.currentCat = ko.observable(new Cat());	
+var Cat = function(data) {
 
-
-	this.incrementCounter = function() {
-		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
-	};
-	
-}
-
-var Cat = function() {
-		//define model data here for now
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.udacity.com/course/viewer#!/c-ud989-nd/l-3406489055/m-3394860566');
-	this.nicknames = ko.observableArray(['Tabtab', 'T-bone', 'Mr.T']);
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observableArray(data.nicknames);
 	this.level = ko.computed(function() {
 		var level;
 		var clicks = this.clickCount();
@@ -30,6 +18,28 @@ var Cat = function() {
 		return level;
 	}, this);
 }
+
+
+var ViewModel = function() {
+	//keep outer this, less confusinsing, sometimes that is used
+	var self= this;
+	//creat all sorts of cats
+	this.currentCat = ko.observable(new Cat({
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		imgAttribution: 'https://www.udacity.com/course/viewer#!/c-ud989-nd/l-3406489055/m-3394860566',
+		nicknames: ['Tabtab', 'T-bone', 'Mr.T']
+	}) );	
+
+
+	this.incrementCounter = function() {
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+	
+}
+//add data so we can add new cat data
+
 
 //apply KO bindings so our function will be run
 
